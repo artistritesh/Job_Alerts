7f840fb30f8eef508228901a5b8277d7a02e56d6
Ritesh Patel <artistritesh@gmail.com>
2025-09-07 13:57:07 +0530

    Changes

--- job_finder.py
+++ job_finder.py
@@ -10,7 +10,7 @@ SERPAPI_KEY = os.getenv("SERPAPI_KEY")
 SENDER_EMAIL = os.getenv("SENDER_EMAIL")
 SENDER_NAME = os.getenv("SENDER_NAME", "Job Alerts Bot")
 SMTP_SERVER = os.getenv("SMTP_SERVER", "smtp.gmail.com")
-SMTP_PORT = int(os.getenv("SMTP_PORT", "587"))  # Gmail uses 587
+SMTP_PORT = int(os.getenv("SMTP_PORT", "587"))
 SMTP_USERNAME = os.getenv("SMTP_USERNAME")
 SMTP_PASSWORD = os.getenv("SMTP_PASSWORD")
 RECIPIENT_EMAILS = os.getenv("RECIPIENT_EMAILS", "")
@@ -48,7 +48,7 @@ def filter_jobs(jobs):
         location = job.get("location", "Unknown")
         link = job.get("apply_options", [{}])[0].get("link", "")
 
-        if not link:  # Skip jobs without a valid application link
+        if not link:
             continue
 
         desc = " ".join([e.get("description", "") for e in job.get("detected_extensions", [])]).lower()
@@ -57,9 +57,6 @@ def filter_jobs(jobs):
             posted_at = datetime.strptime(date_str, "%Y-%m-%d")
         except:
             posted_at = datetime.now()
-
-        if posted_at < cutoff_date:
-            continue
 
         visa_keywords = ["visa", "relocation", "sponsorship", "work permit"]
         visa_support = "Yes" if any(kw in desc for kw in visa_keywords) else "No"
